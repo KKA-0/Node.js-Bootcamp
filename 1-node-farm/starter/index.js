fa4d211687aa4b7e5edb02f2b4b70de316433b49
Karan Agarwal (KKA) <karanyobro@gmail.com>
2023-01-16 20:06:00 +0530

    @

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -2,3 +2,9 @@ const fs = require('fs');
 
 const textin = fs.readFileSync('./txt/input.txt', 'utf-8');
 console.log(textin);
+
+const write = 'HERE we know this:  ${textin}. ${date.now()}';
+fs.writeFileSync('./txt/output.txt', write);
+console.log('Ho gaya write bro');
+
+
