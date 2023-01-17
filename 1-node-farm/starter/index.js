4bbc1e76cae02d493431d57013a9520a417b096c
Karan Agarwal (KKA) <karanyobro@gmail.com>
2023-01-18 01:12:15 +0530

    @

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,10 +1,27 @@
 const fs = require('fs');
+const http = require('http');
+const { Http2ServerResponse } = require('http2');
 
-const textin = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textin);
+/// / / / / /
+//Files
+/// / / / / /
+// const textin = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textin);
 
-const write = 'HERE we know this:  ${textin}. ${date.now()}';
-fs.writeFileSync('./txt/output.txt', write);
-console.log('Ho gaya write bro');
+// const write = 'HERE we know this:  ${textin}. ${date.now()}';
+// fs.writeFileSync('./txt/output.txt', write);
+// console.log('Ho gaya write bro');
 
 
+/// / / / / /
+//SERVER
+/// / / / / /
+
+const server = http.createServer((req, res) =>{
+        res.end('Hello World');
+    }
+        );
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Running');
+})
