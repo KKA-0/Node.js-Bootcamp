7611f687c7d2ccf587f2c05a20f2c3141de59b56
KKA-0 <karanyobro@gmail.com>
2023-02-04 22:34:03 +0530

    @

--- Practice-Projects/Routing/index.js
+++ Practice-Projects/Routing/index.js
@@ -5,16 +5,21 @@ const { type } = require('os');
 
 
 const home = fs.readFileSync(`${__dirname}/templates/index.html`, 'UTF-8');
+const product = fs.readFileSync(`${__dirname}/templates/product.html`, 'UTF-8');
 ///             server created below               ///
 const server = http.createServer((req, res) => {
 
     const { query, pathname } = url.parse(req.url, true);
-    if (pathname === '/' || pathname === '/home') {
+    if (pathname === '/' || pathname === '/home') 
+    {
         res.writeHead(200, { 'Content-type': 'text/html' });
         res.end(home);
     }
-
-
+    else if (pathname === '/product')
+    {
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        res.end(product);
+    }
 });
 
 server.listen(8000, '127.0.1.1', () => {
