83ee5a5c5f9e05db34c1b02b95daa5f41f46f583
KKA-0 <karanyobro@gmail.com>
2023-03-19 02:09:11 +0530

    mongoose started

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,8 +1,41 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
 if(process.env.NODE_ENV === 'development'){
     dotenv.config({path: './config.env'});
 }
-console.log(process.env);
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => console.log("DataBase Connected🎉..."));
+
+const tourSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'A tour must have a name'],
+            unique: true
+        },
+        rating: {
+            type: Number,
+            default: 4.5
+        },
+        price: {
+            type: Number,
+            required: [true, 'A tour must have a price']
+        }
+
+    }
+);
+
+const Tour = mongoose.model('Tour', tourSchema);
+
 const app = require('./app')
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
